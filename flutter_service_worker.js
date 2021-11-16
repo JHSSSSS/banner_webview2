2b959f71622c6a570de86154e2fe50617f109c8d
JHSSSSS <5959song@naver.com>
2021-11-16 16:26:17 +0900

    Revert "v2_2"
    This reverts commit f169054bd71a84b6c9ae1f2c00c53a548b5cd669.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "2bd8ac7a556efc0bfb74b524e002bc0a",
 "index.html": "7f09e653f6d79e639bbd9e8e4c9b50a2",
 "/": "7f09e653f6d79e639bbd9e8e4c9b50a2",
-"main.dart.js": "c96e9afab20a54f978c08fc9ef84d1ec",
+"main.dart.js": "5e38b6223233088547d6bd2edc8f09d9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
